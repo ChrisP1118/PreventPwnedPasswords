0e72431a27e1e84581393f968bb4006a57ba0d2b
ChrisP1118 <cwilson@xaipete.net>
2017-10-27 16:39:01 -0500

    Add files via upload

--- background.js
+++ background.js
@@ -1,42 +1,97 @@
 var pppTestedPasswords = [];
+var whitelistedHostnames = null;
+var learnMoreUrl = 'http://www.xaipete.net/2017/10/24/prevent-pwned-passwords/';
+
+chrome.contextMenus.create({
+	title: 'Clear Whitelist',
+	contexts: ['page_action'],
+	onclick: function () {
+		if (confirm('Are you sure you want to clear the whitelist?'))
+			chrome.storage.sync.clear();
+	}
+});
+
+chrome.contextMenus.create({
+	title: 'Learn more...',
+	contexts: ['page_action'],
+	onclick: function () {
+		chrome.tabs.create({ url: learnMoreUrl });
+	}
+});
+
+function checkHash(hashedPassword, hostname) {
+	console.log(whitelistedHostnames);
+	
+	if (whitelistedHostnames.includes(hostname))
+		return;
+				
+	var notificationId = 'ppp_' + hostname;
+	
+	fetch("https://haveibeenpwned.com/api/v2/pwnedpassword/" + hashedPassword, {
+		cache: 'no-store'
+	}).then(function(response) {
+		if (response.status == 200) {
+		
+			chrome.notifications.onButtonClicked.addListener(function (notificationId, buttonIndex) {
+				if (notificationId == notificationId && buttonIndex == 0) {
+					chrome.tabs.create({ url: learnMoreUrl });
+				} else if (notificationId == notificationId && buttonIndex == 1) {
+					if (!whitelistedHostnames.includes(hostname)) {
+						whitelistedHostnames.push(hostname);
+						chrome.storage.sync.set({'whitelist': whitelistedHostnames});
+						chrome.notifications.clear(notificationId);
+					}
+				}
+			});
+			
+			chrome.notifications.create(notificationId, {
+				type: 'basic',
+				iconUrl: 'ppp_48.png',
+				title: 'Oh no! Pwned password!',
+				message: 'You just used a password on ' + hostname + ' that\'s been present in a data breach. You should consider that password insecure and change it.',
+				buttons: [
+					{title: 'Learn More'},
+					{title: 'Whitelist ' + hostname}
+				],
+				priority: 0});
+				
+		} else if (response.status == 404) {
+			// Password hasn't been pwned -- nothing to do
+		}
+	});
+}
 
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
-		console.log(request);
-		
-		var password = request.password;
-		
-		if (pppTestedPasswords.includes(password)) {
-			return;
-		}
-		
-		pppTestedPasswords.push(password);
-		
-		var xhr = new XMLHttpRequest();
-		xhr.open("GET", "https://haveibeenpwned.com/api/v2/pwnedpassword/" + password, true);
-		xhr.onreadystatechange = function() {
-			if (xhr.readyState == 4) {
-				if (xhr.status == 200) {
+		if (request.action == 'checkPassword') {
+	
+			var password = request.password;
+			if (password == '')
+				return;
+				
+			var hashedPassword = sha1(password);
+			
+			if (pppTestedPasswords.includes(hashedPassword))
+				return;
+			
+			pppTestedPasswords.push(hashedPassword);
+			
+			if (whitelistedHostnames == null) {
+				chrome.storage.sync.get('whitelist', function (results) {
+					if (typeof results.whitelist === 'undefined') {
+						whitelistedHostnames = [];
+						chrome.storage.sync.set({'whitelist': whitelistedHostnames});
+					} else {
+						whitelistedHostnames = results.whitelist;
+					}
 					
-					chrome.notifications.onButtonClicked.addListener(function (notificationId, buttonIndex) {
-						if (notificationId = 'ppp' && buttonIndex == 0)
-							chrome.tabs.create({ url: 'http://www.xaipete.net/2017/10/24/prevent-pwned-passwords/' });
-					});
-					
-					chrome.notifications.create('ppp', {
-						type: 'basic',
-						iconUrl: 'ppp_48.png',
-						title: 'Oh no! Pwned password!',
-						message: 'You just used a password on ' + request.hostname + ' that\'s been present in a data breach. You should consider that password insecure and change it.',
-						buttons: [
-							{title: 'Learn More'}
-						],
-						priority: 0});
-						
-				} else if (xhr.status == 404) {
-					// Password hasn't been pwned -- nothing to do
-				}
+					checkHash(hashedPassword, request.hostname);
+				});
+			} else {
+				checkHash(hashedPassword, request.hostname);
 			}
+		} else if (request.action == 'clearNotification') {
+			var notificationId = 'ppp_' + request.hostname;
+			chrome.notifications.clear(notificationId);
 		}
-		xhr.send();
 	});
--- ppp.js
+++ ppp.js
@@ -1,7 +1,8 @@
-function pppCheckPassword(passwordField) {
+function checkPassword(passwordField) {
 	var password = passwordField.value;
 	
 	chrome.runtime.sendMessage({
+		'action': 'checkPassword',
 		'password': password,
 		'hostname': location.hostname
 	}, function(response) {
@@ -10,13 +11,23 @@ function pppCheckPassword(passwordField) {
 	});	
 }
 
+function clearNotification() {
+	chrome.runtime.sendMessage({
+		'action': 'clearNotification',
+		'hostname': location.hostname
+	});
+}
+
 var passwordFields = document.querySelectorAll('input[type="password"]');
 for (var i = 0; i < passwordFields.length; ++i) {
 	passwordFields[i].addEventListener('keyup', function (e) {
 		if (e.keyCode == 13)
-			pppCheckPassword(this);
+			checkPassword(this);
+		else
+			clearNotification();
 	});
 	passwordFields[i].addEventListener('blur', function (e) {
-		pppCheckPassword(this);
+		checkPassword(this);
 	});
 }
+
